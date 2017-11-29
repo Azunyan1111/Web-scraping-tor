e00eb096a67c622abc93d3718a72db090d8a3346
Azunyan1111 <s15a1302@bene.fit.ac.jp>
2017-11-29 18:35:10 +0900

    add pp

--- main.go
+++ main.go
@@ -3,25 +3,42 @@ package main
 import (
 	"github.com/sclevine/agouti"
 	"log"
+	"sync"
+	"os"
+	"strconv"
 )
 
 func main() {
-	driver := agouti.ChromeDriver(
-		agouti.ChromeOptions("args", []string{"--headless", "--disable-gpu",
-		"--proxy-server=socks5://localhost:9050"}),
-	)
-	if err := driver.Start(); err != nil {
-		log.Fatalf("Failed to start driver:%v", err)
-	}
-	defer driver.Stop()
+	sy := sync.WaitGroup{}
 
-	page, err := driver.NewPage(agouti.Browser("chrome"))
-	if err != nil {
-		log.Fatalf("Failed to open page:%v", err)
-	}
+	i,_ := strconv.Atoi(os.Getenv("MAX_DRIVER"))
 
-	if err := page.Navigate("http://ipinfo.io/"); err != nil {
-		log.Fatalf("Failed to navigate:%v", err)
+	for i > 0{
+		i --
+		sy.Add(1)
+		go func(i int){
+			s := strconv.Itoa(9000 + i)
+			driver := agouti.ChromeDriver(
+				agouti.ChromeOptions("args", []string{"--headless", "--disable-gpu",
+					"--proxy-server=socks5://localhost:" + s}),
+			)
+			if err := driver.Start(); err != nil {
+				log.Fatalf("Failed to start driver:%v", err)
+			}
+			defer driver.Stop()
+
+			page, err := driver.NewPage(agouti.Browser("chrome"))
+			if err != nil {
+				log.Fatalf("Failed to open page:%v", err)
+			}
+
+			if err := page.Navigate("http://ipinfo.io/"); err != nil {
+				log.Fatalf("Failed to navigate:%v", err)
+			}
+			log.Println(page.Title())
+
+			defer sy.Done()
+		}(i)
 	}
-	log.Println(page.Title())
+	sy.Wait()
 }
